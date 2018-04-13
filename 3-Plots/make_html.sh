fa2956d1d14985b41d7340ea3e70c87439f2ca6a
John Vivian <jtvivian@gmail.com>
2018-04-12 23:29:43 -0600

    Extreme spring cleaning: PhD research edition

--- 3-Plots/make_html.sh
+++ 3-Plots/make_html.sh
@@ -2,9 +2,14 @@
 
 FOLDERS=$(ls *.ipynb)
 
+# Enable JT options
+jt -t grade3 -cellw 80%
+
 for py in $FOLDERS; do
 	echo ${py} &&  \
             jupyter nbconvert --to html --template ../jupyter-template/clean_output.tpl \
                 --output-dir html/ ${py};
  done;
 
+# Reset JT options
+jt -r -cellw 80%
