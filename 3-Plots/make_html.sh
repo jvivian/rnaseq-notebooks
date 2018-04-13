a6bb1db9cc37d2f668e77afbe47307f8c8f7e1b9
John Vivian <jtvivian@gmail.com>
2018-04-13 11:01:38 -0600

    Rework notebook to HTML

--- 3-Plots/make_html.sh
+++ 3-Plots/make_html.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 
-FOLDERS=$(ls *.ipynb)
+#FOLDERS=$(ls *.ipynb)
+INPUT=$1
 
 # Enable JT options
-jt -t grade3 -cellw 80%
+jt -t grade3 -cellw 100%
 
-for py in $FOLDERS; do
-	echo ${py} &&  \
-            jupyter nbconvert --to html --template ../jupyter-template/clean_output.tpl \
-                --output-dir html/ ${py};
- done;
+jupyter nbconvert --to html --template ../jupyter-template/clean_output.tpl --output-dir html/ $INPUT
+cite-fix ../jupyter-template/rnaseq_cancer.bib html/"${INPUT%.*}.html"
+
+#echo $INPUT
+#echo "${INPUT%.*}.html"
+
+#for py in $FOLDERS; do
+#	echo ${py} &&  \
+#            jupyter nbconvert --to html --template ../jupyter-template/clean_output.tpl \
+#                --output-dir html/ ${py};
+# done;
+
+#HTML=$(ls html/)
+#for page in $HTML; do
+#	cite-fix ../jupyter-template/rnaseq_cancer.bib html/${page};
+#done;
 
 # Reset JT options
 jt -r -cellw 80%
