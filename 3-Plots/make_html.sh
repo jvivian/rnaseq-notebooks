27f4edc1dfde499605dcb47f5dc1ccc7824f4f2c
John Vivian <jtvivian@gmail.com>
2018-05-17 10:03:14 -0700

    Update scripts

--- 3-Plots/make_html.sh
+++ 3-Plots/make_html.sh
@@ -6,22 +6,12 @@ INPUT=$1
 # Enable JT options
 jt -t grade3 -cellw 100%
 
+# Convert to HTML
 jupyter nbconvert --to html --template ../jupyter-template/clean_output.tpl --output-dir html/ $INPUT
+
+# Convert citations
 cite-fix ../jupyter-template/rnaseq_cancer.bib html/"${INPUT%.*}.html"
-
-#echo $INPUT
-#echo "${INPUT%.*}.html"
-
-#for py in $FOLDERS; do
-#	echo ${py} &&  \
-#            jupyter nbconvert --to html --template ../jupyter-template/clean_output.tpl \
-#                --output-dir html/ ${py};
-# done;
-
-#HTML=$(ls html/)
-#for page in $HTML; do
-#	cite-fix ../jupyter-template/rnaseq_cancer.bib html/${page};
-#done;
 
 # Reset JT options
 jt -r -cellw 80%
+
