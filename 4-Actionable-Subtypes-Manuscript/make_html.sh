6400d47b7a1c12c2ab6a009f284667db12cfaa7e
John Vivian <jtvivian@gmail.com>
2018-05-19 13:37:29 -0700

    Update plot notebooks and script

---
+++ 4-Actionable-Subtypes-Manuscript/make_html.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#FOLDERS=$(ls *.ipynb)
+INPUT=$1
+
+# Enable JT options
+jt -t grade3 -cellw 100%
+
+# Convert to HTML
+jupyter nbconvert --to html --template ../jupyter-template/clean_output.tpl --output-dir html/ $INPUT
+
+# Convert citations
+cite-fix ../jupyter-template/rnaseq_cancer.bib html/"${INPUT%.*}.html"
+
+# Reset JT options
+jt -r -cellw 80%
